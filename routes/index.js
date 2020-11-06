4ee8658a69ec1259f5deb875825bc205225985c8
abhinav098 <agarg10@stevens.edu>
2020-11-06 12:59:34 -0500

    changes in error strategy

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const initRoutes = (app) => {
   });
 
   app.use('*', (req, res) => {
-    res.status(404).render("custom_errors/error", {
+    res.status(404).render("customError", {
       errorReason: "404 Not Found!!!",
       message: "Page you're looking for is not found. Please check your URL"
     });
