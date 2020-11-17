729e1ce24e6d835b416aca327be3879163d20566
abhinav098 <agarg10@stevens.edu>
2020-11-17 17:07:36 -0500

    huskY

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ const initRoutes = (app) => {
   app.use("*", (req, res) => {
     res.status(404).render("customError", {
       errorReason: "404 Not Found!!!",
-      message: "Page you're looking for is not found. Please check your URL",
+      message:
+        "     Page you're looking for is not found. Please check your URL",
     });
   });
 };
