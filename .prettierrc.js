4b9d0b15a99b32bf95b2d1cc384c2f05544f1bc8
abhinav098 <agarg10@stevens.edu>
2020-11-17 17:28:55 -0500

    huskY

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	singleQuote: true,
+	overrides: [
+		{
+			files: '*.hbs',
+			options: {
+				singleQuote: false,
+			},
+		},
+	],
+	trailingComma: 'es5',
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	bracketSpacing: true,
+	htmlWhitespaceSensitivity: 'strict',
+	useTabs: true,
+	printWidth: 80,
+}
