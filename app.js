6b348cc46424cf4582b21d24209cbf75b911d8b2
abhinav098 <agarg10@stevens.edu>
2020-11-17 19:25:26 -0500

    fixing deployment issues

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const app = express();
-const static = express.static(__dirname + '/public');
+const express = require('express')
+const app = express()
+const static = express.static(__dirname + '/public')
+const host = '0.0.0.0'
+const port = process.env.PORT || 3000
 
-const configRoutes = require('./routes');
-const exphbs = require('express-handlebars');
+const configRoutes = require('./routes')
+const exphbs = require('express-handlebars')
 
-app.use('/public', static);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/public', static)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-configRoutes(app);
+configRoutes(app)
 
-app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
-});
+app.listen(port, host, () => {
+  console.log("We've now got a server!")
+  console.log('Your routes will be running on http://localhost:3000')
+})
