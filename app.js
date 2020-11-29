4669f02854a334a76823793489ff67a5ff8b65ed
abhinav098 <agarg10@stevens.edu>
2020-11-28 19:53:42 -0500

    setting up mongoose

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
-
+const mongoose = require('mongoose');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
 
 app.use('/public', static);
 app.use(express.static('public/images'));
@@ -31,9 +30,29 @@ app.engine('handlebars', handlebarsInstance.engine);
 
 app.set('view engine', 'handlebars');
 
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// READY?! Let's go!
+
+// import all of our models
+require('./models');
+
 configRoutes(app);
 
-app.listen(port, host, () => {
+app.listen(process.env.PORT, host, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
