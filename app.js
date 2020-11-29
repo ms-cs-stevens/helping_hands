c52a1590157f54cb279de117150ded10b9e14638
abhinav098 <agarg10@stevens.edu>
2020-11-28 20:01:30 -0500

    adding secret and key in .env

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const session = require('express-session');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 
@@ -11,6 +13,17 @@ app.use('/public', static);
 app.use(express.static('public/images'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: 'HelpingHands',
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 const handlebarsInstance = exphbs.create({
   defaultLayout: 'main',
