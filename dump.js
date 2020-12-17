421529bb41621b76d9c3b31f64d6ad8ad92c8f06
SonaliChavan95 <schava1@stevens.edu>
2020-12-17 00:17:41 -0500

    Update dump

--- dump.js
+++ dump.js
@@ -51,9 +51,34 @@ module.exports = {
         '$2a$16$4o0WWtrq.ZefEmEbijNCGukCezqWTqz1VWlPm/xnaLM8d3WlS5pnK',
       // Password: 'quidditch'
     },
+    {
+      // donor user
+      _id: 'don-583',
+      email: 'donor2@gmail.com',
+      firstname: 'Alex',
+      lastname: 'Perry',
+      gender: 'Female',
+      role_id: 2,
+      hashedPassword:
+        '$2a$16$SsR2TGPD24nfBpyRlBzINeGU61AH0Yo/CbgfOlU1ajpjnPuiQaiDm',
+      // Password: 'damnyoujackdonaghy'
+    },
+    {
+      // recipient user
+      _id: 'rec-583',
+      email: 'recipient2@gmail.com',
+      firstname: 'Peter',
+      lastname: 'Parker',
+      gender: 'Male',
+      role_id: 3,
+      hashedPassword:
+        '$2a$16$4o0WWtrq.ZefEmEbijNCGukCezqWTqz1VWlPm/xnaLM8d3WlS5pnK',
+      // Password: 'quidditch'
+    },
   ],
   donations: [
     {
+      _id: 'dona-234',
       name: 'Center Table',
       quantity: 4,
       description:
@@ -67,6 +92,7 @@ module.exports = {
       status: 'submitted',
     },
     {
+      _id: 'dona-345',
       name: 'Chairs',
       quantity: 1,
       description: 'Cushioned Chairs set of 4.',
@@ -79,6 +105,7 @@ module.exports = {
       status: 'approved',
     },
     {
+      _id: 'dona-654',
       name: 'Office Chairs',
       quantity: 4,
       description: 'Hydraulic Office Chairs with head rest',
@@ -91,6 +118,7 @@ module.exports = {
       status: 'approved',
     },
     {
+      _id: 'dona-136',
       name: 'Sofa',
       quantity: 4,
       description: 'Sofa with cushions',
@@ -103,6 +131,7 @@ module.exports = {
       status: 'submitted',
     },
     {
+      _id: 'dona-736',
       name: 'Table Lamp',
       quantity: 3,
       description: 'Table lamp with bulbs',
@@ -112,6 +141,7 @@ module.exports = {
       status: 'approved',
     },
     {
+      _id: 'dona-536',
       name: 'Floor Mat',
       quantity: 6,
       description: 'Bed Bath and Beyond area rug 37 X 60',
@@ -121,6 +151,7 @@ module.exports = {
       status: 'approved',
     },
     {
+      _id: 'dona-894',
       name: 'Comforter',
       quantity: 8,
       description: 'Utopia Twin XL comforter',
@@ -130,6 +161,7 @@ module.exports = {
       status: 'approved',
     },
     {
+      _id: 'dona-263',
       name: 'Boots',
       quantity: 3,
       description: 'Leather winter boots',
@@ -139,6 +171,7 @@ module.exports = {
       status: 'submitted',
     },
     {
+      _id: 'dona-902',
       name: 'Bed',
       quantity: 10,
       description: 'Bed',
@@ -151,6 +184,7 @@ module.exports = {
       status: 'approved',
     },
     {
+      _id: 'dona-204',
       name: 'Laptop bag',
       quantity: 2,
       description: 'Laptop Bag',
@@ -160,6 +194,7 @@ module.exports = {
       status: 'submitted',
     },
     {
+      _id: 'dona-404',
       name: 'Wooden Chair',
       quantity: 2,
       description: 'Wooden Chair',
@@ -170,6 +205,80 @@ module.exports = {
       zipcode: '07307',
       donor_id: 'don-123',
       status: 'submitted',
+    },
+  ],
+  orders: [
+    {
+      _id: 'ord-1',
+      quantity: 0,
+      items: [],
+      status: 'draft',
+      recipient_id: 'rec-583',
+    },
+    {
+      _id: 'ord-2',
+      quantity: 1,
+      items: [],
+      status: 'placed',
+      recipient_id: 'rec-123',
+    },
+    {
+      _id: 'ord-3',
+      quantity: 2,
+      items: [],
+      status: 'confirmed',
+      recipient_id: 'rec-583',
+    },
+    {
+      _id: 'ord-4',
+      quantity: 3,
+      items: [],
+      status: 'rejected',
+      recipient_id: 'rec-123',
+    },
+  ],
+  items: [
+    {
+      _id: 'itm-1',
+      quantity: 2,
+      recipient_id: 'rec-123',
+      order_id: 'ord-2',
+      donation_id: 'dona-736',
+    },
+    {
+      _id: 'itm-2',
+      quantity: 6,
+      recipient_id: 'rec-123',
+      order_id: 'ord-4',
+      donation_id: 'dona-654',
+    },
+    {
+      _id: 'itm-3',
+      quantity: 2,
+      recipient_id: 'rec-123',
+      order_id: 'ord-4',
+      donation_id: 'dona-902',
+    },
+    {
+      _id: 'itm-4',
+      quantity: 5,
+      recipient_id: 'rec-123',
+      order_id: 'ord-4',
+      donation_id: 'dona-894',
+    },
+    {
+      _id: 'itm-5',
+      quantity: 1,
+      recipient_id: 'rec-583',
+      order_id: 'ord-3',
+      donation_id: 'dona-536',
+    },
+    {
+      _id: 'itm-6',
+      quantity: 6,
+      recipient_id: 'rec-583',
+      order_id: 'ord-3',
+      donation_id: 'dona-902',
     },
   ],
 };
